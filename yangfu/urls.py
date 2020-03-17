378b3ec64072f62ef280fd12b4affe81c5282d84
ryowuandjanet <ryowudata@gmail.com>
2020-03-17 18:01:03 +0800

    修正static路徑及改回bootstrap CSS及JS的設定(由bower改回網址設定)

--- yangfu/urls.py
+++ yangfu/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
 	path('users/', include('users.urls')),
 	path('users/', include('django.contrib.auth.urls')),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
